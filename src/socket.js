6b574a5af587b987ad3f476fd92de05c80ee5e72
YunBins <kig0632@naver.com>
2024-09-12 12:05:10 +0900

    Feat: chatting alarm

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 const SocketIO = require('socket.io');
 const { decode } = require('./modules/jwt/decodeToken');
+const Member = require('./modules/models/member');
 
 module.exports = (server) => {
     const io = SocketIO(server, {
@@ -14,7 +15,7 @@ module.exports = (server) => {
     // 연결된 클라이언트 저장용 객체
     let clients = {}
 
-    io.on('connection', (socket) => { // 웹 소켓 연결 시
+    io.on('connection', async (socket) => { // 웹 소켓 연결 시
         const req = socket.request;
         const ip = req.socket.remoteAddress; // 클라이언트의 ip 주소
         console.log('새로운 클라이언트 접속!', ip, socket.id);
@@ -32,6 +33,23 @@ module.exports = (server) => {
                 clients[member.memberId] = socket.id; // memberId 등록
                 console.log(`${member.memberId}(${socket.id}) 등록됨`);
                 console.log(clients);
+
+                // 사용자의 chat_room_list를 사용하여 해당하는 방들에 join
+                try {
+                    const user = await Member.findById(member.memberId).lean(); // Member 테이블에서 사용자 정보 가져오기
+
+                    if (user && user.chat_room_list && user.chat_room_list.length > 0) {
+                        user.chat_room_list.forEach((roomId) => {
+                            socket.join(roomId.toString()); // 각 채팅방에 소켓 join
+                            console.log(`Socket ${socket.id} joined room ${roomId}`);
+                        });
+                    } else {
+                        console.log(`사용자의 채팅방 목록이 비어있습니다: ${member.memberId}`);
+                    }
+                } catch (err) {
+                    console.error('채팅방 목록을 가져오는 중 오류 발생:', err);
+                }
+
             } else {
                 console.log('토큰에서 memberId를 가져오지 못했습니다.');
             }
@@ -47,24 +65,26 @@ module.exports = (server) => {
 
         // 특정 방에 있는 사용자들에게 메시지 보내기
         socket.on('private message', (msg) => {
-            const { roomId, from, message } = msg;
+            const {roomId, from, message, fromNickname, fromImgUrl} = msg;
 
             // 해당 roomId에 있는 모든 클라이언트에게 메시지 전송
             io.to(roomId).emit('private message', {
                 from, // 보낸 사람 ID
+                fromNickname,
+                fromImgUrl,
                 message, // 메시지 내용
                 roomId // 방 ID
             });
             console.log(`Message from ${from} to room ${roomId}: ${message}`);
         });
 
-        socket.on('disconnect', ()=>{ // 연결 종료 시
+        socket.on('disconnect', () => { // 연결 종료 시
             console.log('클라이언트 접속 해제', ip, socket.id);
             delete clients[socket.id]; // 연결 해제 시 클라이언트 목록에서 제거
             console.log(clients);
         });
 
-        socket.on('error', (error)=> { // 에러 시
+        socket.on('error', (error) => { // 에러 시
             console.error(error);
         });
     });
